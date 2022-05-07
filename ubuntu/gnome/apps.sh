63c1b7d2fa44a6992d9bc025bf646e76ea25e1de
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2022-05-07 17:38:26 +0700

    change ms edge installation method

--- ubuntu/gnome/apps.sh
+++ ubuntu/gnome/apps.sh
@@ -67,7 +67,12 @@ echo -e "\nInstalling Shotwell"
 apt -y install shotwell
 
 # echo -e "\nInstalling Microsoft Edge"
-# pacstall -I microsoft-edge-stable-deb
+# curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+# sudo install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/
+# sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-beta.list'
+# sudo rm microsoft.gpg
+# sudo apt update
+# sudo -y apt install microsoft-edge-stable
 
 # echo -e "\nInstalling  Firefox"
 # apt -y install firefox
