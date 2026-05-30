b7192f1bf6f9c8b328d4742698c2e446bf46e362
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2026-05-30 22:39:37 +0700

    chore: update brew dependencies and cask installations in apps script

--- macos/apps.sh
+++ macos/apps.sh
@@ -1,63 +1,64 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
 brew install git
-
 brew install node
-
+brew install bun
+brew install curl
+brew install ffmpeg
 brew install fnm
-
-brew install yarn
-
 brew install go
-
+brew install k6
+brew install mole
+brew install neovim
 brew install oh-my-posh
+brew install ollama
+brew install openclaw-cli
+brew install opencode
+brew install php
+brew install composer
+brew install rtk
+brew install rust
+brew install rustup
+brew install tailscale
+brew install wget
+brew install yarn
+brew install yt-dlp
 
-brew install --cask arc
-
-brew install --cask chatgpt
-
-brew install --cask discord
-
-brew install --cask docker-desktop
-
-brew install --cask figma
-
-brew install --cask foxitreader
-
-brew install --cask google-chrome 
-
-brew install --cask kindle
-
-brew install --cask microsoft-edge
-
-brew install --cask microsoft-teams
-
-brew install --cask notion
-
-brew install --cask notion-calendar
-
-brew install --cask openvpn-connect
-
-brew install --cask postman
-
-brew install --cask visual-studio-code
-
-brew install --cask zoom
-
-brew install --cask spotify
-
-# brew install --cask maccy
-
-# brew install --cask istat-menus
-
-brew install --cask aldente
-
+brew install --cask android-platform-tools
+brew install --cask antigravity-cli
+brew install --cask antigravity-ide
 brew install --cask apidog
-
+brew install --cask audacity
+brew install --cask blip
+brew install --cask chatgpt
+brew install --cask claude
+brew install --cask claude-code
+brew install --cask cloudflare-warp
+brew install --cask codex
+brew install --cask codex-app
+brew install --cask cursor
 brew install --cask dbeaver-community
-
+brew install --cask discord
+brew install --cask docker-desktop
+brew install --cask figma
+brew install --cask google-chrome
+brew install --cask google-drive
+brew install --cask google-gemini
 brew install --cask herd
-
-# brew install --cask windsurf
-
-# brew install --cask raycast
+brew install --cask iina
+brew install --cask kiro
+brew install --cask kiro-cli
+brew install --cask lm-studio
+brew install --cask microsoft-auto-update
+brew install --cask microsoft-teams
+brew install --cask mongodb-compass
+brew install --cask ngrok
+brew install --cask obs
+brew install --cask obsidian
+brew install --cask openclaw
+brew install --cask opencode-desktop
+brew install --cask openvpn-connect
+brew install --cask tailscale-app
+brew install --cask tradingview
+brew install --cask visual-studio-code
+brew install --cask warp
+brew install --cask zed
+brew install --cask zoom
