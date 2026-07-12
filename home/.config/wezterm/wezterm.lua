51bf9ffdf483307d4a4982be3f1c16862f4509d2
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2026-07-13 01:28:25 +0700

    feat: wezterm initial window size and position

--- home/.config/wezterm/wezterm.lua
+++ home/.config/wezterm/wezterm.lua
@@ -2,6 +2,21 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
+wezterm.on("gui-startup", function(cmd)
+  local screen = wezterm.gui.screens().active
+  local width = 1920
+  local height = 1080
+
+  local tab, pane, window = wezterm.mux.spawn_window(cmd or {})
+  local gui_window = window:gui_window()
+  gui_window:set_inner_size(width, height)
+
+  local x = screen.x + (screen.width - width) / 2
+  local y = screen.y + (screen.height - height) / 2
+  gui_window:set_position(x, y)
+end)
+
+
 config.font = wezterm.font("JetBrainsMono Nerd Font")
 config.font_size = 14.0
 config.window_background_opacity = 0.67
